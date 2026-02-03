fd901d4c410b0b4910e4f351342285ea82017b72
avoy <avoydas0506@gmail.com>
2026-02-04 01:04:57 +0530

    bind ip address and port to the socket

--- main.cpp
+++ main.cpp
@@ -20,7 +20,20 @@ void main() {
         cerr << "Invalid Socket!" << endl;
         return;
     }
+
+    // bind ip address and port to the socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;           // IPv4
+    hint.sin_port = htons(8080);         // The Port (8080)
+    hint.sin_addr.S_un.S_addr = INADDR_ANY; // Any IP address on this machine
+
+    int bindResult = bind(listening, (sockaddr *)&hint, sizeof(hint));
+    if(bindResult == SOCKET_ERROR) {
+        cerr << "Bind failed! Error: " << WSAGetLastError() << endl;
+        closesocket(listening);
+        return;
+    }
     
     WSACleanup();
-    
+
 }
