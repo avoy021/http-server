76986fed82218e408fe0cc1a5d9d5c3c18fe495f
avoy <avoydas0506@gmail.com>
2026-02-06 16:41:15 +0530

    listen and accept client socket connections

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 #pragma comment(lib, "ws2_32.lib")
 
-void main() {
+int main() {
     WSADATA wsaData;
     WORD version = MAKEWORD(2,2);
     
     int wsOk = WSAStartup(version, &wsaData);
     if(wsOk != 0) {
         cout << "Winsock failed to start. Error: " << wsOk << endl;
-        return;
+        return 1;
     }
 
-    //create a socket
+    // create a socket
     SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
     if(listening == INVALID_SOCKET) {
         cerr << "Invalid Socket!" << endl;
-        return;
+        return 1;
     }
 
     // bind ip address and port to the socket
@@ -31,9 +31,21 @@ void main() {
     if(bindResult == SOCKET_ERROR) {
         cerr << "Bind failed! Error: " << WSAGetLastError() << endl;
         closesocket(listening);
-        return;
+        return 1;
+    }
+
+    // tell winsock the socket is for listening
+    listen(listening, SOMAXCONN);
+    
+    // Wait for connections
+    sockaddr_in client;
+    int clientSize = sizeof(client);
+    
+    SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+    if(clientSocket == INVALID_SOCKET) {
+        cerr << "Client Socket is Invalid" << endl;
     }
     
     WSACleanup();
-
+    return 0;
 }
