118aabf6bdf478a861987fe3b2e8e04c89e2a8b4
avoy <avoydas0506@gmail.com>
2026-02-06 20:29:36 +0530

    create echo server using recv() and send() methods, close clientSocket when finished and finally cleanup winsock

--- main.cpp
+++ main.cpp
@@ -64,5 +64,32 @@ int main() {
     // close listening socket
     closesocket(listening);
 
+    // Echo Server: receive data from client and send it back
+    char buf[4096];
+    while(true) {
+        ZeroMemory(buf, 4096);
+        // wait for client to send data
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if(bytesReceived == SOCKET_ERROR) {
+            cerr << "Error in recv(): Quitting" << endl;
+            break;
+        }
+
+        if(bytesReceived == 0) {
+            cout << "Client disconnected" << endl;
+            break;
+        }
+        // print the data
+        cout << "Received: " << string(buf, 0, bytesReceived) << endl;
+        // send message to client
+        send(clientSocket, buf, bytesReceived+1, 0);
+    }
+
+    // close socket
+    closesocket(clientSocket);
+
+    //cleanup winsock
+    WSACleanup();
+
     return 0;
 }
